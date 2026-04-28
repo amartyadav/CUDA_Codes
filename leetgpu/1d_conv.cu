3752f2e36cc70b0e2e650053aab700c1991f6da2
Amartya <amartya.yadav@calligotech.com>
2026-04-28 15:45:24 +0530

    Add 1D convolution implementation using CUDA

---
+++ leetgpu/1d_conv.cu
@@ -0,0 +1,37 @@
+
+
+#include <cuda_runtime.h>
+
+__global__ void convolution_1d_kernel(const float *input, const float *kernel, float *output,
+                                      int input_size, int kernel_size)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int output_size = input_size - kernel_size + 1;
+
+    if(idx >= output_size)
+        return;
+
+    float sum = 0.0f;
+
+    for (int k = 0; k < kernel_size; k++)
+    {
+        float input_value = input[idx+k];
+        float kernel_value = kernel[k];
+        sum += input_value * kernel_value;
+    }
+    
+    output[idx] = sum;
+}
+
+// input, kernel, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float *input, const float *kernel, float *output, int input_size,
+                      int kernel_size)
+{
+    int output_size = input_size - kernel_size + 1;
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (output_size + threadsPerBlock - 1) / threadsPerBlock;
+
+    convolution_1d_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, kernel, output, input_size,
+                                                              kernel_size);
+    cudaDeviceSynchronize();
+}
